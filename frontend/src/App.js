3b517427cfa3fe5ee526b621322b6e54c836f4bb
Harshit Raj <59283222+jainss@users.noreply.github.com>
2023-05-12 22:20:07 +0530

    Frontend Added

---
+++ frontend/src/App.js
@@ -0,0 +1,32 @@
+import "./App.css";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import Banner from "./Components/Banner";
+import Login from './Screens/Login'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import RegisterScreen from "./Screens/Register";
+
+
+function App() {
+  return (
+    <div className='App'>
+      <Router>
+        <header>
+          <Header />
+        </header>
+        <main>
+          <Switch>
+            <Route exact path='/' component={() => <Banner />} />
+            <Route exact path='/login' component={() => <Login />} />
+            <Route exact path='/register' component={() => <RegisterScreen />} />
+          </Switch>
+        </main>
+        <footer>
+          <Footer />
+        </footer>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/Components/Header.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import "./CSS/HeaderS.css";
+import { LinkContainer } from 'react-router-bootstrap'
+
+function Header() {
+    return (
+        <div>
+            <div className="banner">
+                <p>The Ultimate Environment Website !!!!!</p>
+            </div>
+            <div className="container-fluid  header">
+                <div className="heading ">
+                    <LinkContainer to='/'>
+                        <div className="logo">
+                            <p>EcoPlane</p>
+                        </div>
+                    </LinkContainer>
+
+                    <div className="navlist">
+                        <LinkContainer to='/login'>
+                            <ul>
+                                <li >Login</li>
+                            </ul>
+                        </LinkContainer>
+                    </div>
+                </div>
+                <div className="headings1 ">
+                    <ul>
+                        <li><div>Hello Site visitor</div></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
