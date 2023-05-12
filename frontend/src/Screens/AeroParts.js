e15ee4cfced8b2f3ee8837b3dd184ef63144fbe7
SahilJain <sj2394108@gmail.com>
2023-05-13 01:53:35 +0530

    Aeroparts Page Updated

--- frontend/src/Screens/AeroParts.js
+++ frontend/src/Screens/AeroParts.js
@@ -2,6 +2,8 @@ import React from "react";
 import styled from "styled-components";
 import Card from "../Components/Card/Card";
 import { Link } from "react-router-dom";
+
+
 const Container = styled.div`
   padding-top: 30px;
   padding-bottom: 50px;
@@ -17,40 +19,42 @@ const Cards = styled.div`
 `;
 const cardData = [
   {
-    img: "/assets/Fuselage.jpg",
-    title: "Fuselage",
-    description: "Landing gear is a crucial aircraft component that supports and stabilizes the aircraft during takeoff and landing operations."
+    img: "/assets/Avionics.jpeg",
+    title: "Avionics",
+    description: "Avionics refers to electronic systems used in aircraft for navigation, communication, control, and monitoring of various components and functions."
+  },
+  {
+    img: "/assets/Engine.jpg",
+    title: "Engine",
+    description: "An engine is a machine that converts energy into mechanical force, used in vehicles, machinery, and power generation."
   },
   {
     img: "/assets/Fuselage.jpg",
     title: "Fuselage",
+    description: "The fuselage is the main body of an aircraft, housing the crew, passengers, and cargo, and providing structural support"
+  },
+  {
+    img: "/assets/Landing_gear.jpg",
+    title: "Landing Gear",
     description: "Landing gear is a crucial aircraft component that supports and stabilizes the aircraft during takeoff and landing operations."
   },
   {
-    img: "/assets/Fuselage.jpg",
-    title: "Fuselage",
-    description: "Landing gear is a crucial aircraft component that supports and stabilizes the aircraft during takeoff and landing operations."
-  },
-  {
-    img: "/assets/Fuselage.jpg",
-    title: "Fuselage",
-    description: "Landing gear is a crucial aircraft component that supports and stabilizes the aircraft during takeoff and landing operations."
-  },
-  {
-    img: "/assets/Fuselage.jpg",
-    title: "Fuselage",
-    description: "Landing gear is a crucial aircraft component that supports and stabilizes the aircraft during takeoff and landing operations."
+    img: "/assets/Wings.jpg",
+    title: "Wings",
+    description: "Wings are specialized appendages found in birds, insects, and bats that enable flight and are adapted for various functions like gliding or hovering"
   },
 ];
+
+
 const AeroParts = () => {
   return (
     <Container>
-      <Heading>AeroParts</Heading>
+      <Heading style={{ fontWeight: "bold", fontSize: "50px" }}>AeroParts</Heading>
       <Cards>
         {cardData.map((card) => {
           return (
-            <Link to={{pathname: '/part', state: {title: 'title'}}}>
-            <Card img={card.img} title={card.title}  description={card.description} />
+            <Link to={{ pathname: '/part', state: { title: 'title' } }}>
+              <Card img={card.img} title={card.title} description={card.description} />
             </Link>
           );
         })}
