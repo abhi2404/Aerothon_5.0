62799f054f2ec5f965e4084a61878aafad90a7fd
SahilJain <sj2394108@gmail.com>
2023-05-13 14:30:19 +0530

    Ip Updated

--- frontend/src/Config/Config.js
+++ frontend/src/Config/Config.js
@@ -1,3 +1,5 @@
-const api_url = "https://acc7-103-72-7-19.ngrok-free.app/";
-//const api_url = "http://192.168.1.101:8000/"
+// const api_url = "https://acc7-103-72-7-19.ngrok-free.app/";
+
+const api_url = "https://4d43-103-72-7-19.ngrok-free.app/";
+
 export default api_url;
